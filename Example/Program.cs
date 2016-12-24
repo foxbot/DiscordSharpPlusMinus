f450f0f341a68d3051c106e4684df77a1cd5cfc3
Joe Burnard <spongybacon@hotmail.co.uk>
2016-12-24 20:03:08 +0000

    add spongo bongo

--- Example/Program.cs
+++ Example/Program.cs
@@ -8,12 +8,13 @@ class Program
         new Program().Yes();
     }
 
+    // i have done a great good :)
     public void Yes()
     {
         var token = string.Concat("Bot ", Environment.GetEnvironmentVariable("discord-foxboat-token"));
         var client = new DiscordPartyClient(token);
         var channel = client ^ 81384788765712384;
-        var message = channel + "ok it is ready";
+        var message = channel + "hi there Dude :) SMACK SUB BUTTON!!!!! https://www.youtube.com/channel/UCpZdUIxqwnwxvIEmaa1kPqA";
         Console.Read();
     }
 }
